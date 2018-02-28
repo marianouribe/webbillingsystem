a26be2dc9c36341cd59ff534483c3fa459d9d714
Mariano Uribe <umariano@hotmail.com>
2018-02-28 01:38:04 -0400

    menu otra vez

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -41,109 +41,135 @@ class Menu extends Component{
         
     }
 
+
+    // <div className="container-fluid">
+    //                 <nav className = "navbar navbar-toggleable-md navbar-inverse bg-faded"> 
+    //                     <a className = "navbar-brand" href = "#" > 
+    //                         <h2 clasName="navbar-brand mb-0"><i className = "fa fa-desktop"></i>  WB System</h2>
+    //                     </a>
+    //                     <button class="navbar-toggler custom-toggler navbar-toggler-right" 
+    //                         onClick ={()=>this._buttonDropDownClick()}>
+    //                         <span class="navbar-toggler-icon"></span>
+    //                     </button>
+    //                     <form className="form-inline navbar-right my-2 my-lg-0">
+    //                         <div className="form-inline icon-search-custom input">
+    //                             <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
+    //                             <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
+    //                         </div>
+    //                     </form>
+
+    //                 </nav>
+    //             </div>
+
     // <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
     render() {
         return (
             <div>
-                
-                <nav className = "navbar navbar-toggleable-md navbar-inverse bg-faded"> 
-                    <a className = "navbar-brand" href = "#" > 
-                        <h2 clasName="navbar-brand mb-0"><i className = "fa fa-desktop"></i>  WB System</h2>
-                    </a>
-                    <button class="navbar-toggler custom-toggler navbar-toggler-right" 
-                        onClick ={()=>this._buttonDropDownClick()}>
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <form className="form-inline navbar-right my-2 my-lg-0">
-                        <div className="form-inline icon-search-custom input">
-                            <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
-                            <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
+                <form className="form-inline navbar-right my-2 my-lg-0">
+                    <div className="form-inline icon-search-custom input">
+                        <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
+                        <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
+                    </div>
+                </form>
+                <div className="l-site">
+                    <div className="l-nav">
+                        <nav className = {this.state.navbarClassMenu}> 
+                            
+                            <div  id="navbarContentMenu">
+                                <ul id="menu-content" className="navbar-nav">
+                                    <li data-toggle="collapse" data-target="#newFacturacion" >
+                                        <a href="#"><i className="far fas fa-laptop fa-2x"></i>
+                                        <label>Facturación</label></a>
+                                    </li>
+                                    
+                                    <li data-toggle="collapse" data-target="#newCxc">
+                                        <a href="#"><i className="far fa-closed-captioning fa-2x"></i>
+                                        <label>Cuentas Por Cobrar</label></a>
+                                    </li>
+                                    
+                                    <li data-toggle="#" data-target="#newCxcConsulta" >
+                                        <a href="#"><i className="far fa-newspaper fa-2x"></i>
+                                        <label>Consultas</label></a>
+                                    </li>
+                                    
+                                    <li data-target="#newMantenimiento" >
+                                        <a href="#"><i className="fas fa-cogs fa-2x"></i>
+                                        <label>Mantenimiento</label></a>
+                                    </li>
+                                    
+                                    <li data-target="#newSeguridad" >
+                                        <a href="#"><i class="far fa-user fa-2x"></i>
+                                        <label>Seguridad</label></a>
+                                    </li>
+                                    
+                                    <li data-target="#newSalir"  onClick={() => this._handleSalir()}>
+                                        <a href="#"><i className="fas fa-sign-out-alt fa-2x"></i>
+                                        <label>Salir</label></a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="l-page"> 
+                        <section class="band band-a">
+                            <div class="band-container">
+                                <div class="band-inner">
+                                    <div className="container-custom-menu menu-opcion1">
+                                        <h4>Facturacion</h4>
+                                        <hr/>
+                                        <i className="fa fa-money-bill-alt fa-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="band band-a">
+                            <div class="band-container">
+                                <div class="band-inner">
+                                    <div className="container-custom-menu menu-opcion2">
+                                        <h4>Cotizacion</h4>
+                                        <hr/>
+                                        <i className="fa fa-money-bill-alt fa-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="band band-a">
+                            <div class="band-container">
+                                <div class="band-inner">
+                                    <div className="container-custom-menu menu-opcion3">
+                                        <h4>Conduce</h4>
+                                        <hr/>
+                                        <i className="fa fa-money-bill-alt fa-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="band band-a">
+                            <div class="band-container">
+                                <div class="band-inner">
+                                    <div className="container-custom-menu menu-opcion4">
+                                        <h4>Compras</h4>
+                                        <hr/>
+                                        <i className="fa fa-money-bill-alt fa-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div id="newCxc" className="collapse">
+                        
+                        <div className="container-custom-menu menu-opcion1">
+                            <h4>Cobros</h4>
+                            <hr/>
+                            <i className="fa fa-money-bill-alt fa-5x"></i>
                         </div>
-                    </form>
-
-                </nav>
-                
-                <nav className = {this.state.navbarClassMenu}> 
                     
-                    <div  id="navbarContentMenu">
-                        <ul id="menu-content" className="navbar-nav">
-                            <li data-toggle="collapse" data-target="#newFacturacion" >
-                                <a href="#"><i className="far fas fa-laptop fa-2x"></i>
-                                <label>Facturación</label></a>
-                            </li>
-                            
-                            <li data-toggle="collapse" data-target="#newCxc">
-                                <a href="#"><i className="far fa-closed-captioning fa-2x"></i>
-                                <label>Cuentas Por Cobrar</label></a>
-                            </li>
-                            
-                            <li data-toggle="#" data-target="#newCxcConsulta" >
-                                <a href="#"><i className="far fa-newspaper fa-2x"></i>
-                                <label>Consultas</label></a>
-                            </li>
-                            
-                            <li data-target="#newMantenimiento" >
-                                <a href="#"><i className="fas fa-cogs fa-2x"></i>
-                                <label>Mantenimiento</label></a>
-                            </li>
-                            
-                            <li data-target="#newSeguridad" >
-                                <a href="#"><i class="far fa-user fa-2x"></i>
-                                <label>Seguridad</label></a>
-                            </li>
-                            
-                            <li data-target="#newSalir"  onClick={() => this._handleSalir()}>
-                                <a href="#"><i className="fas fa-sign-out-alt fa-2x"></i>
-                                <label>Salir</label></a></li>
-                        </ul>
-                    </div>
-                </nav>
-
-                <div id="newFacturacion" className="collapse">
-                    
-                    <div className="container-custom-menu menu-opcion1">
-                        <h4>Facturacion</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
-                    </div>
-                
-                    <div className="container-custom-menu menu-opcion2">
-                        <h4>Cotizacion</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
-                    </div>
-                
-                    <div className="container-custom-menu menu-opcion3">
-                        <h4>Conduce</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
-                    </div>
-                
-                    <div className="container-custom-menu menu-opcion4">
-                        <h4>Compras</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
+                        <div className="container-custom-menu menu-opcion2">
+                            <h4>Nota de Credito</h4>
+                            <hr/>
+                            <i className="fa fa-money-bill-alt fa-5x"></i>
+                        </div>
                     </div>
                 </div>
-
-                <div id="newCxc" className="collapse">
-                    
-                    <div className="container-custom-menu menu-opcion1">
-                        <h4>Cobros</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
-                    </div>
-                
-                    <div className="container-custom-menu menu-opcion2">
-                        <h4>Nota de Credito</h4>
-                        <hr/>
-                        <i className="fa fa-money-bill-alt fa-5x"></i>
-                    </div>
-                
-                    
-                </div>
-
-                
             </div>
         );
     }
@@ -166,3 +192,15 @@ export default Menu;
 //                         </li>
 //                     </ul> 
 // </div>
+
+
+// <footer className="footer bg-fa-inverse">
+//     <div className="container-fluid btn-primary" >
+//         <div className="row">
+//             <div className="col-sm-12 col-lg-3">
+//                 <i className = "fa fa-desktop fa-3x"></i>
+//             </div>
+//         </div>
+        
+//     </div>
+// </footer>
