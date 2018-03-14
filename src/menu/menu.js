0d07c9885125684ff30435748e7e0c3093284141
Mariano Uribe <umariano@hotmail.com>
2018-03-14 00:41:56 -0400

    mejor project

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -66,51 +66,60 @@ class Menu extends Component{
     render() {
         return (
             <div>
-                <form className="form-inline navbar-right my-2 my-lg-0">
-                    <div className="form-inline icon-search-custom input">
-                        <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
-                        <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
-                    </div>
-                </form>
-                
-                <div className="l-nav">
-                    <nav className = {this.state.navbarClassMenu}> 
-                        <h1>WBSystem</h1>
-                        <div  id="navbarContentMenu">
-                            <ul id="menu-content" className="navbar-nav">
-                                <li data-toggle="collapse" data-target="#newFacturacion" >
-                                    <a href="#"><i className="far fas fa-laptop fa-2x"></i>
-                                    <label>Facturación</label></a>
-                                </li>
-                                
-                                <li data-toggle="collapse" data-target="#newCxc">
-                                    <a href="#"><i className="far fa-closed-captioning fa-2x"></i>
-                                    <label>Cuentas Por Cobrar</label></a>
-                                </li>
-                                
-                                <li data-toggle="#" data-target="#newCxcConsulta" >
-                                    <a href="#"><i className="far fa-newspaper fa-2x"></i>
-                                    <label>Consultas</label></a>
-                                </li>
-                                
-                                <li data-target="#newMantenimiento" >
-                                    <a href="#"><i className="fas fa-cogs fa-2x"></i>
-                                    <label>Mantenimiento</label></a>
-                                </li>
-                                
-                                <li data-target="#newSeguridad" >
-                                    <a href="#"><i class="far fa-user fa-2x"></i>
-                                    <label>Seguridad</label></a>
-                                </li>
-                                
-                                <li data-target="#newSalir"  onClick={() => this._handleSalir()}>
-                                    <a href="#"><i className="fas fa-sign-out-alt fa-2x"></i>
-                                    <label>Salir</label></a></li>
-                            </ul>
+                <div className="title-menu" id="header">
+                    <form className="form-inline navbar-right">
+                        <div className="form-inline icon-search-custom input">
+                            <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
+                            <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
                         </div>
-                    </nav>
+                    </form>
+                    
+                
+                    <div className="container-fluid">
+                        
+                        <div className="l-nav">
+                            <h2>WBSystem</h2>
+                            <nav className = "nav flex-column"> 
+                            
+                                
+                            
+                                <div  id="navbarContentMenu">
+                                    <ul id="menu-content" className="navbar-nav">
+                                        <li data-toggle="collapse" data-target="#newFacturacion" >
+                                            <a href="#"><i className="far fas fa-laptop fa-2x"></i>
+                                            <label>Facturación</label></a>
+                                        </li>
+                                        
+                                        <li data-toggle="collapse" data-target="#newCxc">
+                                            <a href="#"><i className="far fa-closed-captioning fa-2x"></i>
+                                            <label>Cuentas Por Cobrar</label></a>
+                                        </li>
+                                        
+                                        <li data-toggle="#" data-target="#newCxcConsulta" >
+                                            <a href="#"><i className="far fa-newspaper fa-2x"></i>
+                                            <label>Consultas</label></a>
+                                        </li>
+                                        
+                                        <li data-target="#newMantenimiento" >
+                                            <a href="#"><i className="fas fa-cogs fa-2x"></i>
+                                            <label>Mantenimiento</label></a>
+                                        </li>
+                                        
+                                        <li data-target="#newSeguridad" >
+                                            <a href="#"><i class="far fa-user fa-2x"></i>
+                                            <label>Seguridad</label></a>
+                                        </li>
+                                        
+                                        <li data-target="#newSalir"  onClick={() => this._handleSalir()}>
+                                            <a href="#"><i className="fas fa-sign-out-alt fa-2x"></i>
+                                            <label>Salir</label></a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-                <div className="container-fluid l-container-menu">
+                <div className="l-container-menu">
                     <div className="row">
                         <div className="col-sm-12">
                                 <div id="newFacturacion" className="collapse">
