125333b59cab098054287173397587f2ebeec6a1
Mariano Uribe <umariano@hotmail.com>
2018-03-24 11:22:21 -0400

    Configurating billing form

--- src/billing/billing.js
+++ src/billing/billing.js
@@ -30,10 +30,19 @@ class billing extends Component{
     
     <div className="ppagepanel" id = "pagePanelModalBilling" >
       
-        <h1 id="detFacturacion">Detalle de Facturación</h1>
+      <h1 id="detFacturacion">Detalle de Facturación</h1>
+      <div className="w-75">
         <form className="billing-form pt-4">
-            <h4 id="infCliente">Información del Cliente</h4>
+            <h3 id="infCliente">Información del Cliente</h3>
             <div className="">
+              <div className="form-group fg-group w-25 mr-3">
+                <label>Identifación</label>
+                <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
+              </div>
+              <div className="form-group fg-group w-50">
+                <label>Nombre</label>
+                <input type="password" className="form-control" id="NombreCliente" placeholder="Nombre del cliente"/>
+              </div>
               <div className="form-group fg-group w-25 mr-3">
                 <label>Identifación</label>
                 <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
@@ -44,18 +53,58 @@ class billing extends Component{
               </div>
             </div>
         </form>
-    <div>
-      <button id="Save" className="btn btn-outline-primary mr-1">Grabar</button>
-      <Link to = "/">
-        <button id="Close" className="btn btn-outline-primary">Cerrar</button>
-      </Link>
-    </div>
+        <br/>
+        <form className="billing-form pt-4">
+            <h3 id="detFactura">Detalle de factura</h3>
+            <div className="">
+              <div className="form-group fg-group w-25 mr-3">
+                <label>Identifación</label>
+                <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
+              </div>
+              <div className="form-group fg-group w-50">
+                <label>Nombre</label>
+                <input type="password" className="form-control" id="NombreCliente" placeholder="Nombre del cliente"/>
+              </div>
+              <div className="form-group fg-group w-25 mr-3">
+                <label>Identifación</label>
+                <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
+              </div>
+              <div className="form-group fg-group w-50">
+                <label>Nombre</label>
+                <input type="password" className="form-control" id="NombreCliente" placeholder="Nombre del cliente"/>
+              </div>
+            </div>
+        </form>
+        <br/>
+        <form className="billing-form pt-4">
+            <h3 id="detFactura">Forma de Pago</h3>
+            <div className="">
+              <div className="form-group fg-group w-25 mr-3">
+                <label>Identifación</label>
+                <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
+              </div>
+              <div className="form-group fg-group w-50">
+                <label>Nombre</label>
+                <input type="password" className="form-control" id="NombreCliente" placeholder="Nombre del cliente"/>
+              </div>
+              <div className="form-group fg-group w-25 mr-3">
+                <label>Identifación</label>
+                <input type="text" className="form-control" id="IdentificacionCliente" placeholder="Cedula/Pasaport/Rnc"/>
+              </div>
+              <div className="form-group fg-group w-50">
+                <label>Nombre</label>
+                <input type="password" className="form-control" id="NombreCliente" placeholder="Nombre del cliente"/>
+              </div>
+            </div>
+        </form>
+      </div>
+      <div className="mt-4 mb-5">
+        <button id="Save" className="btn btn-outline-primary mr-1">Grabar</button>
+        <Link to = "/">
+          <button id="Close" className="btn btn-outline-primary">Cerrar</button>
+        </Link>
+      </div>
     
-      <br/>
-      <br/>
-        
-
-      
       <Route exact path="/" component={Home} />
     </div>);
   }
