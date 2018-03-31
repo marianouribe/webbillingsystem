cd4afcf74602df104e1684f3b1bc1dad67013adf
Mariano Uribe <umariano@hotmail.com>
2018-03-31 15:16:07 -0400

    reesctructuracion del menu

--- src/header/header.js
+++ src/header/header.js
@@ -4,11 +4,11 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-        <div className="l-nav">
+        <div className="header-custom">
             <div className="title-menu" id="header">
                 <form className="form-inline navbar-right">
                     <div className="form-inline icon-search-custom input">
-                        <i className="fas icon-search fa-search" onClick={()=>this._buttonDropDownClick()}></i>
+                        <i className="fas icon-search fa-search"></i>
                         <input className="form-control mr-sm-2" type="text" placeholder="Buscar"/>                                
                     </div>
                 </form>
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -9,48 +9,118 @@ class NavBar extends Component {
     }
 
   render() {
-    return (
-      <div className="container-fluid">
-        <div className="l-nav">
-            <h2>WB</h2>
-            <h2>System</h2>
-            <nav className = "nav flex-column"> 
-                <div  id="navbarContentMenu">
-                    <ul id="menu-content" className="navbar-nav">
-                        <li data-toggle="collapse" data-target="#newFacturacion" >
-                            <a href="#"><i className="far fas fa-laptop fa-2x"></i>
-                            <label>Facturación</label></a>
-                        </li>
-                        
-                        <li data-toggle="collapse" data-target="#newCxc">
-                            <a href="#"><i className="far fa-closed-captioning fa-2x"></i>
-                            <label>Cuentas Por Cobrar</label></a>
-                        </li>
-                        
-                        <li data-toggle="#" data-target="#newCxcConsulta" >
-                            <a href="#"><i className="far fa-newspaper fa-2x"></i>
-                            <label>Consultas</label></a>
-                        </li>
-                        
-                        <li data-target="#newMantenimiento" >
-                            <a href="#"><i className="fas fa-cogs fa-2x"></i>
-                            <label>Mantenimiento</label></a>
-                        </li>
-                        
-                        <li data-target="#newSeguridad" >
-                            <a href="#"><i class="far fa-user fa-2x"></i>
-                            <label>Seguridad</label></a>
-                        </li>
-                        
-                        <li data-target="#newSalir"  onClick={() => this._handleSalir()}>
-                            <a href="#"><i className="fas fa-sign-out-alt fa-2x"></i>
-                            <label>Salir</label></a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
-    )
+    return <div className="">
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded l-nav navbar-custon toggle-custom">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+          </button>
+          <h2>WB System</h2>
+
+          <div class="collapse navbar-collapse flex-column" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-md-0 flex-column">
+              <li data-toggle="collapse" data-target="#newFacturacion">
+                <a href="#">
+                  <i className="far fas fa-laptop fa-2x" />
+                  <label>Facturación</label>
+                </a>
+              </li>
+
+              <li data-toggle="collapse" data-target="#newCxc">
+                <a href="#">
+                  <i className="far fa-closed-captioning fa-2x" />
+                  <label>Cuentas Por Cobrar</label>
+                </a>
+              </li>
+
+              <li data-toggle="#" data-target="#newCxcConsulta">
+                <a href="#">
+                  <i className="far fa-newspaper fa-2x" />
+                  <label>Consultas</label>
+                </a>
+              </li>
+
+              <li data-target="#newMantenimiento">
+                <a href="#">
+                  <i className="fas fa-cogs fa-2x" />
+                  <label>Mantenimiento</label>
+                </a>
+              </li>
+
+              <li data-target="#newSeguridad">
+                <a href="#">
+                  <i class="far fa-user fa-2x" />
+                  <label>Seguridad</label>
+                </a>
+              </li>
+
+              <li data-target="#newSalir" onClick={() => this._handleSalir()}>
+                <a href="#">
+                  <i className="fas fa-sign-out-alt fa-2x" />
+                  <label>Salir</label>
+                </a>
+              </li>
+            </ul>
+            {/* <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search" />
+              <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
+                Search
+              </button>
+            </form> */}
+          </div>
+        </nav>
+
+        {/* <div className="l-nav navbar-custon">
+          <h2>WB</h2>
+          <h2>System</h2>
+          <nav className="nav flex-column">
+            <div id="navbarContentMenu">
+              <ul id="menu-content" className="navbar-nav">
+                <li data-toggle="collapse" data-target="#newFacturacion">
+                  <a href="#">
+                    <i className="far fas fa-laptop fa-2x" />
+                    <label>Facturación</label>
+                  </a>
+                </li>
+
+                <li data-toggle="collapse" data-target="#newCxc">
+                  <a href="#">
+                    <i className="far fa-closed-captioning fa-2x" />
+                    <label>Cuentas Por Cobrar</label>
+                  </a>
+                </li>
+
+                <li data-toggle="#" data-target="#newCxcConsulta">
+                  <a href="#">
+                    <i className="far fa-newspaper fa-2x" />
+                    <label>Consultas</label>
+                  </a>
+                </li>
+
+                <li data-target="#newMantenimiento">
+                  <a href="#">
+                    <i className="fas fa-cogs fa-2x" />
+                    <label>Mantenimiento</label>
+                  </a>
+                </li>
+
+                <li data-target="#newSeguridad">
+                  <a href="#">
+                    <i class="far fa-user fa-2x" />
+                    <label>Seguridad</label>
+                  </a>
+                </li>
+
+                <li data-target="#newSalir" onClick={() => this._handleSalir()}>
+                  <a href="#">
+                    <i className="fas fa-sign-out-alt fa-2x" />
+                    <label>Salir</label>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div> */}
+      </div>;
   }
 };
 
